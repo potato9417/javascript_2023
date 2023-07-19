b82702c2ff96c0ca0eae9df88d629496cb65cd89
potato9417 <59465217+potato9417@users.noreply.github.com>
2023-07-19 17:36:12 +0900

    0719_클릭이벤트만 추가
    버튼눌렀을때 클릭이벤트 콘솔추가

--- 0719/index.js
+++ 0719/index.js
@@ -1,7 +1,8 @@
-// 슬라이드이미지 넣기
 let slides = document.querySelectorAll("ul li");
+let btnPrev = document.querySelector(".prev");
+let btnNext = document.querySelector(".next");
 
-// console.log(slides);
+// 슬라이드이미지 넣기
 for(slide of slides){
     // console.log(slide.getAttribute("id"));
     let slideId = slide.getAttribute("id");
@@ -12,3 +13,10 @@ for(slide of slides){
     `
 }
 
+btnPrev.addEventListener("click",()=>{
+    console.log("click-prev");
+})
+
+btnNext.addEventListener("click",()=>{
+    console.log("click-next");
+})
