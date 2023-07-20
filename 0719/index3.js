7991f35b20d383b66e72f64774e53d705840443d
potato9417 <59465217+potato9417@users.noreply.github.com>
2023-07-20 17:56:09 +0900

    0720_for문 함수화하는 과정 오류부분 해결
    선생님의 도움으로 해결 다시 확인 후 정리 예정

---
+++ 0719/index3.js
@@ -0,0 +1,68 @@
+let slide = document.querySelector("ul");
+let btnPrev = document.querySelector(".prev");
+let btnNext = document.querySelector(".next");
+let slides = [];
+
+// 슬라이드이미지 넣는 함수
+// function putImg(){
+//     for(i=0;i<5;i++){
+//         slides.push(i+1);
+//         slide.innerHTML +=
+//         `   
+//             <li id = ${slides[i]}>
+//                 <img src="./images/${slides[i]}.jpg">
+//             </li>
+//         `
+//     }
+// }
+
+// javateacher : 2023.7.20
+function putImg(status){
+
+    console.log("slides : " + slides);
+
+    for(i=0;i<5;i++){
+
+        // javateacher : 2023.7.20
+        // 화면 초기화할 때만 배열에 요소 추가
+        if (status == 'init') {
+            slides.push(i+1);
+        }
+
+        slide.innerHTML +=
+        `   
+            <li id = ${slides[i]}>
+                <img src="./images/${slides[i]}.jpg">
+            </li>
+        `
+    }
+}
+
+// 웹실행시 이미지 나열하기
+putImg('init'); // javateacher : 2023.7.20
+
+// 이전버튼 클릭시
+btnPrev.addEventListener("click",()=>{ 
+    slide.innerHTML="";
+
+    let item = slides[0];
+    slides.shift(); // 베열의 첫번째값 제거
+    slides.push(item) // 배열 끝에 값 추가
+
+    console.log("이전버튼 클릭 "+slides)
+
+    putImg()
+})
+
+// 다음버튼 클릭시
+btnNext.addEventListener("click",()=>{
+    slide.innerHTML="";
+    
+    let item = slides[slides.length-1];
+    slides.pop(); // 배열 마지막값 제거
+    slides.unshift(item) // 배열 맨앞에 값 추가
+    
+    console.log("다음버튼 클릭 "+slides)
+
+    putImg()
+})
