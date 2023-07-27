0197ee10c7a0e3e990c49e7e5a629ab62350c021
potato9417 <59465217+potato9417@users.noreply.github.com>
2023-07-27 17:37:17 +0900

    0727_입력값 확인 및 버틀클릭 함수 재생성 , 간단한 css수정

--- 0725/index.js
+++ 0725/index.js
@@ -11,11 +11,41 @@ let birth = document.querySelector("#birth");
 let submitBtn = document.querySelector("#submit");
 let resetBtn = document.querySelector("#reset");
 
-let idResult = document.querySelector("#id_box .result");
+// 라디오의 체크된 값 불러오기
+let genderVal = document.querySelector('.gender_box .form_box input[name="gender"]:checked');
 
+// 입력값에 따른 결과 값
+let idResult = document.querySelector("#id_box .result");
+let pwResult = document.querySelector("#pw_box .result");
+let nameResult = document.querySelector("#name_box .result");
+let emailResult = document.querySelector("#email_box .result");
+let phoneResult = document.querySelector("#phone_box .result");
+
+// 결과 값 노출
 let num =1;
 let userInfo = [];
+let pushInfo={};
 
+// 결과값에 따른 boolean
+let idIsError = false;
+let pwIsError = false;
+let nameIsError = false;
+let emailIsError = false;
+let phoneIsError = false;
+
+// 정규식
+const regexId = /^[a-zA-Z]{1}\w{7,19}$/;
+    // /^ $/ : 정규식의 시작과 끝을 나타냄
+    // [a-zA-Z]{1} : 앞글자는 영대소문자로 시작해야한다는 정규식
+    // \w : 영대소문자 & 숫자 & 밑줄문자 => [A-z0-9__]와 같은 뜻 
+    // {7,19} : 8~20자리 글자
+    // 앞글자는 영대소문자로 시작하는 8~20자리의 글자
+const regexPw = /^(?=.*[a-zA-Z])((?=.*\d)(?=.*\W)).{8,20}$/;
+const regexName = /^[가-힣]{2,4}$/;
+const regexEmail = /^[a-zA-Z0-9_+.-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,4}$/;
+const regexPhone = /^(\d{3}).*(\d{3}).*(\d{4})$/;
+
+// 리셋
 function resetInfo(){
     id.value = "";
     pw.value = "";
@@ -28,76 +58,91 @@ function resetInfo(){
     birth.value = "";
 }
 
+// 정보 추출
+function submitInfo(){
+    num+1;
+    pushInfo = {
+        "key":num,
+        "id":id.value,
+        "pw":pw.value,
+        "userName":userName.value,
+        
+        "email":email.value,
+        "phone":phone.value,
+    }
+    console.log(genderVal)
+    userInfo.push(pushInfo)
+    console.log(userInfo)
+    resetInfo();
+}
+
+// 오류일 경우 표시
+function errorBlock(error){
+    if(error) {
+        console.log(error)
+    }
+    
+}
+
 // 취소버튼 클릭시 모든 입력값 제거
 resetBtn.addEventListener("click",resetInfo())
 
-// 가입버튼 클릭시 값 확인하는 정규식
-submitBtn.addEventListener("click",()=>{
+// // 가입버튼 클릭시 값 확인하는 정규식
+// submitBtn.addEventListener("click",()=>{
 
-    // 입력값을 확인하는 정규식
-    let regexId = /[a-zA-Z]{1}\w{7,19}/;
-    let regexPw = /(?=.*[a-zA-Z])((?=.*\d)(?=.*\W)).{8,20}/;
-    let regexName = /[가-힣]{2,4}/;
-    let regexEmail = /[a-zA-Z0-9_+.-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,4}/;
-    let regexPhone = /(\d{3}).*(\d{3}).*(\d{4})/;
+//     // 정규식.test(확인할값) => 결과값 : true/false
+//     if(regexId.test(id.value) == false){
+//        console.log("아이디를 올바르게 입력해주세요");
+//     }
+//     else {
+//         if(regexPw.test(pw.value) == false){
+//             console.log("비밀번호를 올바르게 입력해주세요");
+//         }
+//         else {
+//             if(regexName.test(userName.value) == false){
+//                 console.log("한글 이름을 올바르게 입력해주세요");
+//             }
+//             else {
+//                 if(regexEmail.test(email.value) == false){
+//                     console.log("이메일을 올바르게 입력해주세요");
+//                 }
+//                 else {
+//                     if(regexPhone.test(phone.value) == false){
+//                         console.log("핸드폰번호를 올바르게 입력해주세요")
+//                     }
+//                     else {
+//                         console.log("가입완료되었습니다");
+//                         num+1;
+//                         userInfo = {
+//                             "key":num,
+//                             "id":id.value,
+//                             "pw":pw.value,
+//                             "userName":userName.value,
+//                             "email":email.value,
+//                             "phone":phone.value,
+//                         }
+//                         console.log(userInfo)
+//                         resetInfo()
+//                     }
+//                 }
 
-    // 정규식.test(확인할값) => 결과값 : true/false
-    if(regexId.test(id.value) == false){
-       console.log("아이디를 올바르게 입력해주세요");
-    }
-    else {
-        if(regexPw.test(pw.value) == false){
-            console.log("비밀번호를 올바르게 입력해주세요");
-        }
-        else {
-            if(regexName.test(userName.value) == false){
-                console.log("한글 이름을 올바르게 입력해주세요");
-            }
-            else {
-                if(regexEmail.test(email.value) == false){
-                    console.log("이메일을 올바르게 입력해주세요");
-                }
-                else {
-                    if(regexPhone.test(phone.value) == false){
-                        console.log("핸드폰번호를 올바르게 입력해주세요")
-                    }
-                    else {
-                        console.log("가입완료되었습니다");
-                        num+1;
-                        userInfo = {
-                            "key":num,
-                            "id":id.value,
-                            "pw":pw.value,
-                            "userName":userName.value,
-                            "email":email.value,
-                            "phone":phone.value,
-                        }
-                        console.log(userInfo)
-                        resetInfo()
-                    }
-                }
+//             }
 
-            }
+//         }
 
-        }
-
-    }
-})
+//     }
+// })
 
 
 // input입력 후 이벤트 처리
 id.onblur =()=>{ // .onblur => 포커스가 없어졌을때 이벤트발생
-    let regexId = /[a-zA-Z]{1}\w{7,19}/;
-    // [a-zA-Z]{1} : 앞글자는 영대소문자로 시작해야한다는 정규식
-    // \w : 영대소문자 & 숫자 & 밑줄문자 => [A-z0-9__]와 같은 뜻 
-    // {7,19} : 8~20자리 글자
-    // 앞글자는 영대소문자로 시작하는 8~20자리의 글자
     
     if(regexId.test(id.value) == false){
         console.log("아이디를 올바르게 입력해주세요");
 
         idResult.innerText = "아이디를 올바르게 입력해주세요";
         idResult.style.color = "#f00";
+        idIsError = true;
 
     }
     else {
@@ -105,3 +150,96 @@ id.onblur =()=>{ // .onblur => 포커스가 없어졌을때 이벤트발생
         idResult.innerText = "아이디를 입력하셨습니다";
     }
 }
+
+pw.onblur =()=>{ // .onblur => 포커스가 없어졌을때 이벤트발생
+    
+    if(regexPw.test(pw.value) == false){
+        console.log("비밀번호를 올바르게 입력해주세요");
+
+        pwResult.innerText = "비밀번호를 올바르게 입력해주세요";
+        pwResult.style.color = "#f00";
+        pwIsError = true;
+
+
+    }
+    else {
+        pwResult.style.color = "#999";
+        pwResult.innerText = "비밀번호를 입력하셨습니다";
+    }
+}
+
+userName.onblur =()=>{ // .onblur => 포커스가 없어졌을때 이벤트발생
+    
+    if(regexName.test(userName.value) == false){
+        console.log("이름을 올바르게 입력해주세요");
+
+        nameResult.innerText = "이름을 올바르게 입력해주세요";
+        nameResult.style.color = "#f00";
+        nameIsError = true;
+
+
+    }
+    else {
+        nameResult.style.color = "#999";
+        nameResult.innerText = "이름을 입력하셨습니다";
+    }
+}
+
+email.onblur =()=>{ // .onblur => 포커스가 없어졌을때 이벤트발생
+    
+    if(regexEmail.test(email.value) == false){
+        console.log("이메일을 올바르게 입력해주세요");
+
+        emailResult.innerText = "이메일을 올바르게 입력해주세요";
+        emailResult.style.color = "#f00";
+        emailIsError = true;
+
+
+    }
+    else {
+        emailResult.style.color = "#999";
+        emailResult.innerText = "이메일을 입력하셨습니다";
+    }
+}
+
+phone.onblur =()=>{ // .onblur => 포커스가 없어졌을때 이벤트발생
+    
+    if(regexPhone.test(phone.value) == false){
+        console.log("번호를 올바르게 입력해주세요");
+
+        phoneResult.innerText = "번호를 올바르게 입력해주세요";
+        phoneResult.style.color = "#f00";
+        phoneIsError = true;
+
+    }
+    else {
+        phoneResult.style.color = "#999";
+        phoneResult.innerText = "번호를 입력하셨습니다";
+    }
+}
+
+// 가입버튼클릭시
+submitBtn.addEventListener("click",()=>{
+    // button 클릭시 새로고침 현상발생
+    // button의 type을 submit이 아닌 button으로 변경하거나
+    // button을 감싸고 있는 부모 태그를 form이 아닌 div로 변경
+    // => submit으로 백단으로 보내야 하므로 두번째 방법 사용
+    // https://devhoma.tistory.com/90
+
+    if( regexId.test(id.value),
+        regexPw.test(pw.value),
+        regexName.test(userName.value),
+        regexEmail.test(email.value),
+        regexPhone.test(phone.value) == false) {
+
+        console.log("값을 다시 입력해주세요");
+        errorBlock(idIsError);
+        resetInfo();
+    }
+    else {
+        submitInfo();
+    }
+    // submitInfo();
+});
+    
+
